d039a320c85a79b02cec1b2e01e97389a91fd0ff
AlexM-O-Leao <65860878+AlexM-O-Leao@users.noreply.github.com>
2020-06-27 18:33:42 +0100

    First Commit

---
+++ src/main/java/inventory/models/ProductClass.java
@@ -0,0 +1,82 @@
+package inventory.models;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name="products")
+public class ProductClass implements Product {
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private int id;
+    private String productName;
+    private float price;
+    private int qtd;
+
+    public ProductClass(String productName) {
+        this.productName = productName;
+        this.price = 0;
+        this.qtd = 1;
+    }
+
+    public ProductClass(String productId, String productName) {
+        this(productName);
+        this.id = Integer.parseInt(productId);
+    }
+
+    public ProductClass() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String prd_productName) {
+        this.productName= prd_productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public JsonObjectBuilder getJsonObjectBuilder() {
+        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+        jsonObjectBuilder.add("id", this.getId());
+        jsonObjectBuilder.add("name", this.getProductName());
+        jsonObjectBuilder.add("price", this.getPrice());
+        jsonObjectBuilder.add("quantity", this.getQtd());
+        return jsonObjectBuilder;
+    }
+
+
+    public boolean equals(Object obj) {
+        if(obj instanceof Product) {
+            return this.getId() == ((Product)obj).getId();
+        }
+        return super.equals(obj);
+    }
+}
